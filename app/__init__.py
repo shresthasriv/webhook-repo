627cf82391df5327f9d86339d1295818798ffe8a
shresthasriv <shresthas04.ms@gmail.com>
2025-07-06 06:47:53 +0530

    added mongodb support

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 
 from app.webhook.routes import webhook
+from app.extensions import init_db
 
 
 # Creating our flask app
 def create_app():
 
     app = Flask(__name__)
+    
+    # Enable CORS for all routes
+    CORS(app)
+    
+    # Initialize MongoDB
+    init_db(app)
     
     # registering all the blueprints
     app.register_blueprint(webhook)
--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,10 @@
 from flask_pymongo import PyMongo
 
 # Setup MongoDB here
-# mongo = PyMongo(uri="mongodb://localhost:27017/database")
+mongo = PyMongo()
+
+def init_db(app):
+    mongo_uri = 'mongodb+srv://loverofdogs2004:test@techstax.ni0ef3w.mongodb.net/github_webhooks?retryWrites=true&w=majority&appName=techstax'
+    app.config['MONGO_URI'] = mongo_uri
+
+    mongo.init_app(app)
